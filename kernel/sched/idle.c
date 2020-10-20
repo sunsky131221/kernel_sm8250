93a53b9f54d9e78af4a3158381532af54e1c61cf
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2020-10-20 09:47:14 -0400

    sched: membarrier: cover use_mm (v4)
    Add comments and memory barrier to use_mm and unuse_mm
    to allow the effect of membarrier(2) to apply to kthreads accessing
    user-space memory as well.
    
    Given that no prior kthread use this guarantee and that it only affects
    kthreads, adding this guarantee does not affect user-space ABI.
    
    Refine the check in membarrier_global_expedited to exclude runqueues
    running the idle thread rather than all kthreads from the IPI cpumask.
    
    Now that membarrier_global_expedited can IPI kthreads, the scheduler
    also needs to update the runqueue's membarrier_state when entering lazy
    TLB state.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20201020134715.13909-3-mathieu.desnoyers@efficios.com
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -327,6 +327,7 @@ void play_idle(unsigned long duration_ms)
 	WARN_ON_ONCE(!(current->flags & PF_KTHREAD));
 	WARN_ON_ONCE(!(current->flags & PF_NO_SETAFFINITY));
 	WARN_ON_ONCE(!duration_ms);
+	WARN_ON_ONCE(current->mm);
 
 	rcu_sleep_check();
 	preempt_disable();
